07c79f5061ad45a12a5cb9deddd0ece0598c7ebd
Alberto J Garcia <albertojg1991@gmail.com>
2026-03-16 21:43:17 -0700

    Added function that applies LoRA.

---
+++ src/lora_fine_tune/lora_utils.py
@@ -0,0 +1,12 @@
+from peft import LoraConfig, get_peft_model
+
+def apply_lora(model):
+    config = LoraConfig(
+        r=8,
+        lora_alpha=16,
+        target_modules=["c_attn"],  # GPT-2 attention projection
+        lora_dropout=0.1,
+        bias="none",
+        task_type="CAUSAL_LM",
+    )
+    return get_peft_model(model, config)
